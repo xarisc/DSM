6ad8570a5c7b7f98a810cbb1a5e1bd542e637716
SophieSchulze <schulze.sophie@gmx.net>
2019-11-10 23:49:28 +0100

    litte problem

--- src/DatenBankVerwaltung.java
+++ src/DatenBankVerwaltung.java
@@ -1,2 +1,7 @@
 public class DatenBankVerwaltung {
+  
+  huhu guten Morgen,
+  bekomm das mit dem verbinden nicht hin. hab da was zu gefunden
+    https://www.jetbrains.com/help/idea/connecting-to-a-database.html
+  laut dem ist das nur in der ultimate edition möglich
 }
