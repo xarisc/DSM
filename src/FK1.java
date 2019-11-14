e4e9a4ea4cf37de02598e008328ad4856069c20c
jschulz <j.schulz@vr-now.org>
2019-11-14 10:12:23 +0100

    Fachkonzept Interface

--- src/FK1.java
+++ src/FK1.java
@@ -1,5 +1,6 @@
-public class FK1 {
-
-
-
+public abstract class FK1 implements IFK {
+    @Override
+    public void InitialisiereSpeichermedium(String speichermedium) {
+        
+    }
 }
--- src/IFK.java
+++ src/IFK.java
@@ -1,2 +1,26 @@
 public interface IFK {
+    void InitialisiereSpeichermedium(String speichermedium);
+
+    void LeseHausbesitzer(int Hausbesitzer_ID);
+    void SpeichereNeuenHausbesitzer(String Hausbesitzer_Name);
+    void AendereHausbesitzer(int Hausbesitzer_ID, String Hausbesitzer_Name);
+    void LoescheHausbesitzer(int Hausbesitzer_ID);
+    int GetHausbesitzer_ID();
+    String GetHausbesitzer_Name();
+    int GetAnzahlHausbesitzer();
+
+    void LeseImmobilie(int Immobilie_ID);
+    void LoescheImmobilie(int Immobilie_ID);
+    void AendereImmobilie(int Immobilie_ID, String Immobilie_Adresse, float Immobilie_Preis, boolean Immobilie_Typ, float Immobilie_Flaeche, int Immobilie_Raeume);
+    void ErstelleImmobilie(String Immobilie_Adresse, float Immobilie_Preis, boolean Immobilie_Typ, float Immobilie_Flaeche, int Immobilie_Raeume);
+    int GetImmobilie_ID();
+    String GetImmobilie_Adresse();
+    float GetImmobilie_Preis();
+    boolean GetImmobilie_Typ();
+    float GetImmobilie_Flaeche();
+    int GetImmobilie_Raeume();
+    int GetAnzahlImmobilie();
+
+    void AendereHausbesitzer();
+    int GetHausbesitzer();
 }
